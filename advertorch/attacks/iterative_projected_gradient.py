2f28db857878b9d4442b65e8278f37ae80b081d1
cathalsmyth <47541126+cathalsmyth@users.noreply.github.com>
2019-05-16 16:43:39 -0400

    Update iterative_projected_gradient.py

--- advertorch/attacks/iterative_projected_gradient.py
+++ advertorch/attacks/iterative_projected_gradient.py
@@ -87,7 +87,7 @@ def perturb_iterative(xvar, yvar, predict, nb_iter, eps, eps_iter, loss_fn,
     x_adv = clamp(xvar + delta, clip_min, clip_max)
     result_mag=torch.sqrt(torch.sum(x_adv**2))
 
-    #x_adv=x_adv/result_mag*radius
+    x_adv=(x_adv/result_mag)*radius
     return x_adv
 
 
